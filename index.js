5e4f218de0d4c3350f18c32b82f9baba83316b21
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-08-18 22:19:27 -0300

    refactor: remove unnecessary import

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const ejs = require('ejs')
 const { PrismaClient } = require('@prisma/client')
 const path = require("path");
 const prisma = new PrismaClient()
