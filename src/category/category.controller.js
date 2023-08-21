1d95e3f5449e3c2f79eb976b743f310fb8d08679
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-08-21 11:42:25 -0300

    feat: add edit route

--- src/category/category.controller.js
+++ src/category/category.controller.js
@@ -46,4 +46,38 @@ router.post('/categories/:id', async (req, res) => {
     res.redirect("/categories")
 })
 
+router.get('/categories/:id', async (req, res) => {
+    if(!req.params.id || req.params.id === '') {
+        res.redirect('/categories')
+        return;
+    }
+
+    const category = await prisma.category.findUnique({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.render('admin/categories/edit', { category: category })
+})
+
+router.post('/categories/:id/edit', async (req, res) => {
+    if(!req.params.id || req.params.id === '') {
+        res.redirect('/categories')
+        return;
+    }
+
+    await prisma.category.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            title: req.body.title,
+            slug: slugify(req.body.title, { lower: true })
+        }
+    })
+
+    res.redirect('/categories')
+})
+
 module.exports = router
