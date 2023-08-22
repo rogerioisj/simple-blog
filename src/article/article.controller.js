929b2a19a00292ab054a8ed483fd346c9a5f6099
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2023-08-22 11:33:22 -0300

    feat: add route to add and list articles

--- src/article/article.controller.js
+++ src/article/article.controller.js
@@ -4,12 +4,45 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const slugify = require('slugify')
 
-router.get('/admin/article/new', (req, res) => {
-    res.render('admin/articles/new');
+router.get('/admin/article/new', async (req, res) => {
+    const categories = await prisma.category.findMany();
+
+    res.render('admin/articles/new', { categories: categories });
 })
 
 router.post('/article', async (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    let categoryId = req.body.category;
 
+    if (!title || !content || !categoryId) {
+        res.redirect('/admin/article/new')
+    }
+
+    await prisma.article.create({
+        data: {
+            title: title,
+            content: content,
+            slug: slugify(title),
+            category: {
+                connect: {
+                    id: categoryId
+                }
+            }
+        }
+    })
+
+    res.redirect('/articles')
+})
+
+router.get('/articles', async (req, res) => {
+    const articles = await prisma.article.findMany({
+        include: {
+            category: true
+        }
+    })
+
+    res.render('admin/articles/list', { articles: articles })
 })
 
 module.exports = router
